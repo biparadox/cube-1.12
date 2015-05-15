f8d7ebc78ce1d5a5c8d095038df96789b130a8dd
Hu jun <algorist@bjut.edu.cn>
2015-05-16 07:56:37 +0800

    new progess

--- general/router_process_func.c
+++ general/router_process_func.c
@@ -43,7 +43,39 @@ int proc_router_hit_target(void * message,char * local_uuid,char * proc_name)
 
 }
 
+int 	proc_audit_init()
+{
+	char audit_text[4096];
+        const char * audit_filename= "./message.log";
+    	int fd ;
+	char *beginstr="\n**************Begin new trust node proc****************************\n";
+    	fd=open(audit_filename,O_WRONLY|O_CREAT|O_APPEND);
+    	if(fd<0)
+		return -ENOENT;
+	write(fd,beginstr,strlen(beginstr));
+	close(fd);
+	return 0;
+}
 
+int     proc_audit_log (void * message)
+{
+	int ret;
+	char *isostr="\n************************************************************\n";
+	char audit_text[4096];
+        const char * audit_filename= "./message.log";
+    	int fd ;
+	ret=message_2_json(message,audit_text);	
+
+	audit_text[ret]='\n';			
+    	fd=open(audit_filename,O_WRONLY|O_APPEND);
+    	if(fd<0)
+		return -ENOENT;
+	write(fd,audit_text,ret+1);
+	write(fd,isostr,strlen(isostr));
+	close(fd);
+}
+
+			
 int proc_router_recv_msg(void * message,char * local_uuid,char * proc_name)
 {
 	void * sec_sub;
@@ -90,7 +122,53 @@ int proc_router_send_msg(void * message,char * local_uuid,char * proc_name)
 	int ret;
 	MESSAGE_HEAD * msg_head;
 	BYTE conn_uuid[DIGEST_SIZE*2];
+	switch(message_get_state(message))
+	{
+		case MSG_FLOW_LOCAL:
+		case MSG_FLOW_ASPECT_LOCAL:
+		
+			msg_head=get_message_head(message);
+			if(msg_head==NULL)
+			{
+				return  -EINVAL;
+			}
+			ret=find_sec_subject(msg_head->receiver_uuid,&sec_sub);	
+			if(sec_sub!=NULL)
+			{
+				if(sec_subject_getprocstate(sec_sub)<SEC_PROC_START)
+				{	
+					printf("start process %s!\n",sec_subject_getname(sec_sub));
+    					ret=sec_subject_start(sec_sub,NULL);
+				}
+				send_sec_subject_msg(sec_sub,message);
+				printf("send message to local process %s!\n",msg_head->receiver_uuid);
+			}
+			break;
 
+		case MSG_FLOW_DELIVER:
+		case MSG_FLOW_QUERY:
+		case MSG_FLOW_RESPONSE:
+		case MSG_FLOW_ASPECT:
+		case MSG_FLOW_ASPECT_RETURN:
+		
+			ret=find_sec_subject("connector_proc",&sec_sub);	
+			if(sec_sub==NULL)
+			{
+				printf("can't find conn process!\n");
+				return -EINVAL;
+			}
+			send_sec_subject_msg(sec_sub,message);
+			printf("send message to conn process!\n");
+				
+			break;
+		default:
+			return -EINVAL;
+
+	}
+
+
+
+/*	
 	if((message_get_state(message) & MSG_FLOW_LOCAL)
 		|| (message_get_flow(message) & MSG_FLOW_ASPECT_LOCAL))
 	{
@@ -149,6 +227,7 @@ int proc_router_send_msg(void * message,char * local_uuid,char * proc_name)
 	{
 		return -EINVAL;
 	}
+	*/
 	return 0;
 }
 
@@ -170,6 +249,7 @@ int proc_router_init(void * sub_proc,void * para)
     ret=sec_subject_getcontext(sub_proc,&context);
     if(ret<0)
 	return ret;
+    proc_audit_init();
     return 0;
 }
 
@@ -200,16 +280,11 @@ int proc_router_start(void * sub_proc,void * para)
 	char local_uuid[DIGEST_SIZE*2+1];
 	char proc_name[DIGEST_SIZE*2+1];
 	char receiver_uuid[DIGEST_SIZE*2+1];
-	char *isostr="\n************************************************************\n";
 	BYTE conn_uuid[DIGEST_SIZE*2];
 	
 	ret=proc_share_data_getvalue("uuid",local_uuid);
 	ret=proc_share_data_getvalue("proc_name",proc_name);
 
-	char audit_text[4096];
-        const char * audit_filename= "./message.log";
-    	int fd =open(audit_filename,O_CREAT|O_RDWR|O_TRUNC);
-        close(fd);
 
 
 //	struct timeval time_val={0,10*1000};
@@ -348,6 +423,7 @@ int proc_router_start(void * sub_proc,void * para)
 					send_state=STATE_ASPECT;
 					break;
 				case MSG_FLOW_ASPECT:
+					// first, check if the aspect router reach the target
 					hit_target=proc_router_hit_target(message,local_uuid,proc_name);
 					if(hit_target<0)
 					{
@@ -356,16 +432,18 @@ int proc_router_start(void * sub_proc,void * para)
 					}
 					else if(hit_target==0)
 					{
+						// if aspect router do not reach the target, push the node's uuid and try to find the local aspect policy
 						send_state=STATE_ASPECT_LOCAL;
-						router_push_site(message,conn_uuid,"ASPE");
+						router_push_site(message,conn_uuid,"APRE");
 						break;
 					}
-					// query message reach the target, so we should cancel the query flow and add the response flow,
-					// and set state to local to wait a local plugin to deal with the message  
+					// if aspect router reach the target,set flow to ASPECT_RETURN, set state to ASPECT_LOCAL
+					// pop an address in aspect stack and try to find the local aspect policy
+					// if can't find an aspect local policy, this message will be discarded   
 					message_set_state(message,MSG_FLOW_ASPECT_LOCAL);
 					flow=message_get_flow(message);
 					message_set_flow(message,(flow&(~MSG_FLOW_ASPECT))|(MSG_FLOW_ASPECT_RETURN));
-					send_state=MSG_FLOW_ASPECT_LOCAL;
+					send_state=STATE_ASPECT_LOCAL;
 					break;
 				case MSG_FLOW_ASPECT_RETURN:
 					ret=router_check_sitestack(message,"APRE");
@@ -429,7 +507,7 @@ int proc_router_start(void * sub_proc,void * para)
 				switch(send_state)
 				{
 					case STATE_LOCAL:
-						ret=router_find_match_policy(message,&msg_policy,sec_subject_getname(sub_proc));
+						ret=router_find_local_policy(message,&msg_policy,sec_subject_getname(sub_proc));
 						if(ret<0)
 						{
 							send_state=STATE_ERROR;
@@ -456,10 +534,12 @@ int proc_router_start(void * sub_proc,void * para)
 						}
 						if(msg_policy==NULL)
 						{
-							printf("message %s is discarded!\n",message_get_recordtype(message));
+							printf("message %s is discarded in SEND step!\n",message_get_recordtype(message));
 							send_state=STATE_ERROR;
 							break;
 						}
+						if(router_policy_gettype(msg_policy)&MSG_FLOW_QUERY)
+							router_push_site(message,conn_uuid,"FTRE");
 						ret=router_set_main_flow(message,msg_policy);
 						if(ret<0)
 						{
@@ -475,11 +555,22 @@ int proc_router_start(void * sub_proc,void * para)
 							send_state=STATE_ERROR;
 							break;
 						}
-						if(msg_policy==NULL)
+						if(aspect_policy==NULL)
 						{
 							send_state=STATE_ASPECT;
 							break;
 						}
+						flow=message_get_flow(message);
+						if(flow & MSG_FLOW_ASPECT_RETURN)
+						{
+							ret=router_pop_site(message,"APRE");
+							if(ret<0)
+							{
+								send_state=STATE_ERROR;
+								break;
+							}
+						}
+						router_push_site(message,message_get_receiver(message),"APRE");
 						ret=router_set_aspect_flow(message,aspect_policy);
 						if(ret<0)
 						{
@@ -498,20 +589,54 @@ int proc_router_start(void * sub_proc,void * para)
 						if(aspect_policy==NULL)
 						{
 							int flow=message_get_flow(message);
+							if(flow & MSG_FLOW_ASPECT)
+							{
+								message_set_state(message,MSG_FLOW_ASPECT_RETURN);
+								send_state=STATE_TRANS;
+								break;
+							}
 							if(flow & MSG_FLOW_RECV)
 							{
 								message_set_state(message,MSG_FLOW_FINISH);
 								send_state=STATE_FINISH;
 								break;
 							}
+							if(flow & MSG_FLOW_RESPONSE)
+							{
+								message_set_state(message,MSG_FLOW_RESPONSE);
+							}
+							else if(flow & MSG_FLOW_QUERY)
+							{
+								message_set_state(message,MSG_FLOW_QUERY);
+							}
+							send_state=STATE_TRANS;
+							break;
 
 						}
+						// find an aspect policy, we need set the aspect flow flag
+						message_set_state(message,MSG_FLOW_ASPECT);
+						// check if the aspect stack exist
+						ret=router_check_sitestack(message,"APRE");
+						if(ret<0)
+						{
+							send_state=STATE_ERROR;
+							break;
+						}
+						if(ret==0)
+						{
+							// if the aspect stack not exist, we should push message's receiver id first
+							router_push_site(message,message_get_receiver(message),"APRE");
+						}
+						// push current trust node's uuid
+						router_push_site(message,conn_uuid,"APRE");
+						// set the aspect policy 
 						ret=router_set_aspect_flow(message,aspect_policy);
 						if(ret<0)
 						{
 							send_state=STATE_ERROR;
 							break;
 						}
+						// prepare to trans
 						send_state=STATE_TRANS;
 						break;
 					case STATE_ASPECT_RETURN:
@@ -539,7 +664,7 @@ int proc_router_start(void * sub_proc,void * para)
 							}
 							send_state=STATE_TRANS;
 						}
-						printf("message %s is discarded!\n",message_get_recordtype(message));
+						printf("message %s is discarded in FINISH state!\n",message_get_recordtype(message));
 						send_state=STATE_ERROR;
 						break;
 					default:
@@ -549,6 +674,7 @@ int proc_router_start(void * sub_proc,void * para)
 			}
 			if(send_state==STATE_ERROR)
 			{
+				proc_audit_log(message);
 				message_free(message);
 				continue;
 			}
@@ -563,27 +689,14 @@ int proc_router_start(void * sub_proc,void * para)
 						break;
 					if(dup_msg!=NULL)
 					{
-						ret=message_2_json(dup_msg,audit_text);	
-						audit_text[ret]='\n';			
-    						fd=open(audit_filename,O_WRONLY|O_CREAT|O_APPEND);
-    						if(fd<0)
-	  						return -ENOENT;
-						write(fd,audit_text,ret+1);
-						close(fd);
+						proc_audit_log(message);
 						proc_router_send_msg(dup_msg,local_uuid,proc_name);
 					}
 					router_rule=router_get_next_duprule(msg_policy);
 				}
 			}
-			ret=message_2_json(message,audit_text);	
 
-			audit_text[ret]='\n';			
-    			fd=open(audit_filename,O_WRONLY|O_CREAT|O_APPEND);
-    			if(fd<0)
-	  			return -ENOENT;
-			write(fd,audit_text,ret+1);
-			write(fd,isostr,strlen(isostr));
-			close(fd);
+			proc_audit_log(message);
 
 			ret=proc_router_send_msg(message,local_uuid,proc_name);
 			if(ret<0)
