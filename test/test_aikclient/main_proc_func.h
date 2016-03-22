b30c6855df7f0fe70f7a7086c93c72001bee17fa
Hu Jun <algorist@bjut.edu.cn>
2016-03-21 18:30:34 -0700

    add test_aikclient

---
+++ test/test_aikclient/main_proc_func.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_PROC_FUNC_H
+#define MAIN_PROC_FUNC_H
+#include "../include/vtpm_struct.h"
+#include "../include/vtpm_desc.h"
+#include "aik_process_func.h"
+
+int test_bind_init();
+
+// aik_casign plugin's init func and kickstart func
+
+#endif
