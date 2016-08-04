68f90bb27d008f2e91cdfdda5721f42ecea471ce
Hu jun <algorist@bjut.edu.cn>
2016-08-04 00:08:35 -0700

    move msghub to MsgHub

---
+++ TrustChat/src/hub/MsgHub/MsgHub_func.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <dirent.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include "../include/data_type.h"
+#include "../include/struct_deal.h"
+#include "../include/crypto_func.h"
+#include "../include/extern_struct.h"
+#include "../include/extern_defno.h"
+#include "../include/valuename.h"
+#include "../include/message_struct.h"
+#include "../include/message_struct_desc.h"
+#include "../include/vmlist.h"
+#include "../include/vtpm_struct.h"
+#include "../include/vm_policy.h"
+#include "../include/connector.h"
+#include "../include/logic_baselib.h"
+#include "../include/connector.h"
+#include "../include/sec_entity.h"
+#include "../include/main_proc_init.h"
+
+#include "main_proc_func.h"
+
+int MsgHub_init()
+{
+	int ret;
+	return 0;
+}
